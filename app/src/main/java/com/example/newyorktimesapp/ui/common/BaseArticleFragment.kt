62a8f3427663b3e7ce3dfa7193bcc65aa4d5f60f
Mark Rott <markrott@gmail.com>
2020-09-15 13:16:40 +0300

    Added enter/exit animation

--- app/src/main/java/com/example/newyorktimesapp/ui/common/BaseArticleFragment.kt
+++ app/src/main/java/com/example/newyorktimesapp/ui/common/BaseArticleFragment.kt
@@ -2,11 +2,21 @@ package com.example.newyorktimesapp.ui.common
 
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.example.newyorktimesapp.KEY_URL
 import com.example.newyorktimesapp.R
 
-abstract class BaseArticleFragment: Fragment() {
+abstract class BaseArticleFragment : Fragment() {
+
+    private val navOptions = NavOptions.Builder()
+        // logic of the opening screen
+        .setEnterAnim(R.anim.slide_in_right)
+        .setPopExitAnim(R.anim.slide_out_right)
+        // logic of closing the screen
+        .setExitAnim(R.anim.fade_out)
+        .setPopEnterAnim(R.anim.fade_in)
+        .build()
 
     protected val articlesAdapter: ArticleAdapter = ArticleAdapter { data ->
         when (data) {
@@ -18,7 +28,7 @@ abstract class BaseArticleFragment: Fragment() {
 
     private fun openCommentsFragment(data: ArticleClickPayload.CommentsAction) {
         val bundle = bundleOf(KEY_URL to data.model.url)
-        findNavController().navigate(R.id.commentsFragment, bundle)
+        findNavController().navigate(R.id.commentsFragment, bundle, navOptions)
     }
 
     abstract fun favoriteAction(data: ArticleClickPayload.FavoriteAction)
